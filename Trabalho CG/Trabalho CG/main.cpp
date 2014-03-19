24e8f855d44cf063340c61a3ecd5e43899d50e90
noslenfy <ndcmota@gmail.com>
2014-03-19 16:47:44 +0000

    Aterado o motor

--- Trabalho CG/Trabalho CG/main.cpp
+++ Trabalho CG/Trabalho CG/main.cpp
@@ -8,18 +8,24 @@
 #include "classes.h"
 #include <GL/glut.h>
 
+
+int font1 = (int)GLUT_BITMAP_HELVETICA_12;
+
+
 float lookatx=0, lookaty=0, lookatz=5.0; // Não usado
 float rotateAng=0.0;
 float rotateTop=0.0;
 
-bool linhas = false;  // usado para o menu
+int linhas = 0;  // 0 -> linhas; 1 -> opaco; 3-> pontos  usado para o menu
 
 
 float raiocamara = 5;  // raio rotação
 float alfa = 0;  // usado para rotação da câmara
-float beta = 0;  // usado para rotação da câmara
+float beta = 0.5;  // usado para rotação da câmara
+float zoom = 1.0;  // zoom inicial
+float zoomInc = 0.05;  //incremento zoom
 
-
+int w = 70, h = 70; // usado para setOrthographicProjection
 
 // class que contem a primitiva
 scene cena;
@@ -73,15 +79,56 @@ void drawScene()
 	}
 }
 
+void resetPerspectiveProjection() {
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+}
+void setOrthographicProjection() {
+
+	// switch to projection mode
+	glMatrixMode(GL_PROJECTION);
+
+	// save previous matrix which contains the 
+	//settings for the perspective projection
+	glPushMatrix();
+
+	// reset matrix
+	glLoadIdentity();
+
+	// set a 2D orthographic projection
+	gluOrtho2D(0, w, 0, h);
+
+	// invert the y axis, down is positive
+	glScalef(1, -1, 1);
+
+	// move the origin from the bottom left corner
+	// to the upper left corner
+	glTranslatef(0, -h, 0);
+
+	// switch back to modelview mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+void renderSpacedBitmapString( float x,	float y, int spacing, void *font, char *string) {
+
+	char *c;
+	int x1 = x;
+
+	for (c = string; *c != '\0'; c++) {
+
+		glRasterPos2f(x1, y);
+		glutBitmapCharacter(font, *c);
+		x1 = x1 + spacing;
+	}
+}
+
 
 void renderScene(void) {
 
 	// clear buffers
-	if (linhas==true)
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	else
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// set the camera
@@ -90,14 +137,39 @@ void renderScene(void) {
 		0.0, 0.0, 0.0,
 		0.0f, 1.0f, 0.0f);
 
+	glPushMatrix();
+	glPopMatrix();
 
-	// pôr instruções de desenho aqui
 
-	glRotatef(rotateAng,0,1,0);
-	glRotatef(rotateTop,1,0,0);
+	//inserir instruções de desenho
+	glScalef(zoom, zoom, zoom);
+
+	switch (linhas) {
+	case '0': glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); break;
+	case '1': glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); break;
+	case '2': glPolygonMode(GL_FRONT_AND_BACK, GL_POINT); break;
+	}
+
+
+	// Permite rodar objecto NÂO UTILIZADO
+	glRotatef(rotateAng, 0, 1, 0);
+	glRotatef(rotateTop, 1, 0, 0);
 
 	// Realiza o deseho conforme class cena
 	drawScene();
+
+
+	setOrthographicProjection();
+
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1, 1, 1);
+
+	renderSpacedBitmapString(1, 2, 1, (void*)font1, " Zoom in: <a>   Zoom out: <z>   Camara: <up> <down> <left> <right>");
+
+	glPopMatrix();
+
+	resetPerspectiveProjection();
 
 	// End of frame
 	glutSwapBuffers();
@@ -109,12 +181,7 @@ void sK(int codigo, int x, int y)
 	{
 		switch(codigo)
 		{
-		case GLUT_KEY_HOME:
-			lookatx=lookatx+0.3;
-			break;
-		case GLUT_KEY_END:
-			lookatx=lookatx-0.3;
-			break;
+
 		case GLUT_KEY_RIGHT:
 			alfa = alfa + 0.087;
 			break;
@@ -136,17 +203,11 @@ void kP(unsigned char codigo, int x, int y)
 	{
 		switch(codigo)
 		{
-		case 'e':
-			lookatx=lookatx+0.3;
+		case 'a':
+			zoom += zoomInc;
 			break;
-		case 'q':
-			lookatx=lookatx-0.3;
-			break;
-		case 'w':
-			lookaty=lookaty+0.3;
-			break;
-		case 's':
-			lookaty=lookaty-0.3;
+		case 'z':
+			zoom -= zoomInc;
 			break;
 		}
 		glutPostRedisplay();
@@ -158,11 +219,13 @@ void menu(int opcao) {
 		switch (opcao)
 		{
 		case 1:
-			linhas = true;
+			linhas = 0;
 			break;
 		case 2:
-			linhas = false;
+			linhas = 1;
 			break;
+		case 3:
+			linhas = 2;
 		}
 		glutPostRedisplay();
 	}
@@ -237,6 +300,7 @@ int main(int argc, char **argv) {
 	int i = glutCreateMenu(menu);
 	glutAddMenuEntry("Linhas",1);
 	glutAddMenuEntry("Opaco",2);
+	glutAddMenuEntry("Pontos",3);
 
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
